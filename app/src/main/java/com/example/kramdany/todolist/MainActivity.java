683d710caf6212fa0ee60759209020358370bbf5
Kieran Ramdany <kieran.ramdany@ynap.com>
2018-01-17 12:00:53 +0000

    Create Android app with empty activity - Add view and code for basic in memory todo list

---
+++ app/src/main/java/com/example/kramdany/todolist/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.kramdany.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout todoList;
+    private EditText editText;
+    private Button addButton;
+
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            todoList = this.findViewById(R.id.todoList);
+            addButton = this.findViewById(R.id.addButton);
+            editText = this.findViewById(R.id.editText);
+            editText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+                    if(editable.toString().isEmpty()){
+                        addButton.setEnabled(false);
+                    }
+                    else {
+                        addButton.setEnabled(true);
+                    }
+                }
+            });
+        }
+
+    public void addTodo(View view) {
+        Log.i("buttonClickedViewId", Integer.toString(view.getId()));
+        String text = editText.getText().toString().trim();
+        if(!text.isEmpty()) {
+            LayoutInflater layoutInflater = LayoutInflater.from(this.getApplicationContext());
+            TextView todoItem = (TextView) layoutInflater.inflate(R.layout.todo_item, todoList, false);
+            todoItem.setText(editText.getText());
+            editText.setText("");
+            todoList.addView(todoItem);
+        }
+        else {
+            editText.setText("");
+        }
+
+
+    }
+}
